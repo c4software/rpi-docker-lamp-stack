32482013360e896e2e2376893c9d4321c0bb995d
Valentin Brosseau <c4software@gmail.com>
2021-01-23 22:32:10 +0000

    Add users list folder

--- html/index.php
+++ html/index.php
@@ -9,17 +9,20 @@
     <h1>Welcome home</h1>
     <p>Please upload your files using your favorite FTP Software.</p>
 
+    <hr />
     <h2>Current user(s) base directory</h2>
     <ul>
         <?php
-            $dirs = scandir("./users/");
+            $dirs = array_diff(glob('users/*', GLOB_ONLYDIR),  array('..', '.'));
 
-            foreach ($dirs as $dirs) {
-                echo "<li><a href='/~$dir'>~$dir</a></li>";
+	    foreach ($dirs as $dir) {
+		    $dir = basename($dir);
+                echo "<li><a href='/~$dir'>/~$dir/</a></li>";
             }
         ?>
     </ul>
 
+    <hr />
     <h2>This Docker Stack include</h2>
     <ul>
         <li>PHP 7</li>
