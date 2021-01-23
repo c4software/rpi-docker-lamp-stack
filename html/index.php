cd68bec3fa95f9b3def2d0d30cea0aa181f1ec3c
Valentin Brosseau <c4software@gmail.com>
2021-01-23 23:25:50 +0100

    List directories

--- html/index.php
+++ html/index.php
@@ -9,6 +9,17 @@
     <h1>Welcome home</h1>
     <p>Please upload your files using your favorite FTP Software.</p>
 
+    <h2>Current user(s) base directory</h2>
+    <ul>
+        <?php
+            $dirs = scandir("./users/");
+
+            foreach ($dirs as $dirs) {
+                echo "<li><a href='/~$dir'>~$dir</a></li>";
+            }
+        ?>
+    </ul>
+
     <h2>This Docker Stack include</h2>
     <ul>
         <li>PHP 7</li>
